af5baf1bb507e2e10a8f5f47f838bdf204b7bc46
hamin kang <kyden5386@gmail.com>
2023-03-01 23:49:04 +0900

    0301 commit(2)

---
+++ src/main/java/com/bluedragontrain/bluedragon/config/auth/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.bluedragontrain.bluedragon.config.auth;
+
+import com.bluedragontrain.bluedragon.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnDefaultWebSecurity efaultWebSecurity
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class SecurityConfig {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().headers().frameOptions().disable()
+                .and().authorizeHttpRequests()
+                .requestMatchers("/", "/css/**", "/images/**", "/h2-console/**").permitAll()
+                .requestMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and().logout().logoutSuccessUrl("/")
+                .and().oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
+        return http.build();
+    }
+}
