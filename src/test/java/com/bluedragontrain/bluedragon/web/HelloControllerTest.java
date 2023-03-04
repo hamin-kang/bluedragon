5e629afeb5cc969d3814252b5a1e206f1d321ef3
hamin kang <kyden5386@gmail.com>
2023-03-04 22:50:31 +0900

    0304 commit

--- src/test/java/com/bluedragontrain/bluedragon/web/HelloControllerTest.java
+++ src/test/java/com/bluedragontrain/bluedragon/web/HelloControllerTest.java
@@ -1,10 +1,14 @@
 package com.bluedragontrain.bluedragon.web;
 
 
+import com.bluedragontrain.bluedragon.config.auth.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.is;
@@ -14,13 +18,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class) // JUnit 에 내장된 실행자 외에 다른 실행자 실행, 스프링 실행자
-@WebMvcTest(controllers = HelloController.class) // 여러 스프링 어노테이션 중, Web 에 집중할 수 있는 어노테이션이다.
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+}) // 여러 스프링 어노테이션 중, Web 에 집중할 수 있는 어노테이션이다.
 public class HelloControllerTest {
     @Autowired // 스프링이 관리하는 빈을 주입받는다.
     private MockMvc mvc; // 웹 API 테스트 할 때 사용, 스프링 MVC 테스트의 시작점
 
+    @WithMockUser(roles = "USER")
     @Test
-    public void return_Hello() throws Exception {
+    public void Hello가_리턴된다() throws Exception {
         String hello = "hello";
 
         mvc.perform(get("/hello")) // MockMvc 를 통해 /hello 주소로 GET 요청을 한다.
@@ -28,8 +35,9 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello)); // Controller 에서 "hello"를 리턴하기 때문에 이 값이 맞는지 검증
     }
 
+    @WithMockUser(roles = "USER")
     @Test
-    public void return_helloDto() throws Exception {
+    public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
 
