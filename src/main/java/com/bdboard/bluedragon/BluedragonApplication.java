f89336ee980232ca5e6c3e963f43fbfb4281245c
hamin-kang <kyden5386@gmail.com>
2025-02-19 21:21:33 +0900

    initial commit

---
+++ src/main/java/com/bdboard/bluedragon/BluedragonApplication.java
@@ -0,0 +1,13 @@
+package com.bdboard.bluedragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluedragonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluedragonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bdboard/bluedragon/BluedragonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdboard.bluedragon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluedragonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
