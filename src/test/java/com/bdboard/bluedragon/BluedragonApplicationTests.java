e0f88c3a51836acfaa8314126758d7fc76342063
hamin-kang <kyden5386@gmail.com>
2025-02-21 17:30:40 +0900

    junit 테스트

--- src/test/java/com/bdboard/bluedragon/BluedragonApplicationTests.java
+++ src/test/java/com/bdboard/bluedragon/BluedragonApplicationTests.java
@@ -1,6 +1,8 @@
 package com.bdboard.bluedragon;
 
-import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,21 +11,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BluedragonApplicationTests {
 	
-	@Autowired
+	@Autowired // DI(Dependency Injection): 의존성 주입, QuestionRepositroy 객체 주입.
 	private QuestionRepository questionRepository;
 	
 	@Test
 	void testJpa() {
-		Question q1 = new Question();
-		q1.setSubject("트럼프 2기의 무역정책");
-		q1.setContent("트럼프 2기의 무역정책에 대해 자세히 알려주실 수 있을까요?");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1); // 첫번째 질문 저장
+		List<Question> all = this.questionRepository.findAll(); // SELECT * FROM QUESTION
+		assertEquals(2, all.size());
 		
-		Question q2 = new Question();
-		q2.setSubject("스프링 부트");
-		q2.setContent("스프링 부트가 뭔지, 어떻게 사용하는지 알려주실 분?!");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);
+		Question q = all.get(0);
 	}
 }
