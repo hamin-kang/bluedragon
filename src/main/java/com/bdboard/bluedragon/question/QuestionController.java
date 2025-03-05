37d03dd31f48e169a5107fab1eb063b681393774
hamin <kyden5386@gmail.com>
2025-03-05 22:18:05 +0900

    질문 답변 기능 완성

--- src/main/java/com/bdboard/bluedragon/question/QuestionController.java
+++ src/main/java/com/bdboard/bluedragon/question/QuestionController.java
@@ -5,18 +5,28 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor // final 또는 @NonNull 필드에 대해 생성자를 자동으로 생성해줌.
 @Controller
 public class QuestionController {
 	private final QuestionService questionService;
 	
-	@GetMapping("/question/list")
+	@GetMapping("/list")
 	public String list(Model model) {
 		List<Question> questionList = this.questionService.getList();
 		model.addAttribute("questionList", questionList);
 		return "question_list";
+	}
+	
+	@GetMapping(value = "/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		Question question = this.questionService.getQuestion(id);
+		model.addAttribute("question", question);
+		return "question_detail";
 	}
 }
