c1412e7497a60255cb48bc600ecebaed3a89ad59
hamin <kyden5386@gmail.com>
2025-03-10 23:21:29 +0900

    로그인 기능구현

--- src/main/java/com/bdboard/bluedragon/user/UserRepository.java
+++ src/main/java/com/bdboard/bluedragon/user/UserRepository.java
@@ -1,7 +1,9 @@
 package com.bdboard.bluedragon.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
-	
+	Optional<SiteUser> findByusername(String username);
 }
